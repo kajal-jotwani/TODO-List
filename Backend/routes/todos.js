66da21826765e720a416cc676c06d2b2c84a0b8e
kajal-jotwani <kajaljotwani06@gmail.com>
2024-10-19 18:22:03 +0530

    added delete requeest

--- Backend/routes/todos.js
+++ Backend/routes/todos.js
@@ -1,50 +1,59 @@
-const {Todo} = require("../models/todo");
+const { Todo } = require("../models/todo");
 const express = require("express");
 const Joi = require("joi");
 
-
 const router = express.Router();
 
-router.get("/", async(req, res) => {
-    try {
-        const todos = await Todo.find().sort({date: -1})
-        res.send(todos)
-    } catch(error){
-        res.status(500).send(error.message);
-        console.log(error.message)
-    }
-})
-
-router.post("/", async(req, res) =>{
-    const Schema  = Joi.object({
-        name: Joi.string().min(3).max(300).required(),
-        author: Joi.string().min(3).max(30),
-        uid: Joi.string(),
-        isComplete: Joi.boolean(),
-        date: Joi.date() 
-    })
-
-    const {error} = Schema.validate(req.body)
-
-    if(error) return res.status(400).send(error.details[0].message)
-   
-    const {name, author, isComplete, date, uid} = req.body;
-
-    let todo = new Todo({
-        name,
-        author,
-        isComplete,
-        date,
-        uid
-    });
-    try{
-        todo = await todo.save();
-        res.send(todo);
-    }
-    catch(error){
-        res.status(500).send(error.message);
-        console.log(error.message)
-    }
+router.get("/", async (req, res) => {
+  try {
+    const todos = await Todo.find().sort({ date: -1 });
+    res.send(todos);
+  } catch (error) {
+    res.status(500).send(error.message);
+    console.log(error.message);
+  }
 });
 
-module.exports = router
+router.post("/", async (req, res) => {
+  const Schema = Joi.object({
+    name: Joi.string().min(3).max(300).required(),
+    author: Joi.string().min(3).max(30),
+    uid: Joi.string(),
+    isComplete: Joi.boolean(),
+    date: Joi.date(),
+  });
+
+  const { error } = Schema.validate(req.body);
+
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const { name, author, isComplete, date, uid } = req.body;
+
+  let todo = new Todo({
+    name,
+    author,
+    isComplete,
+    date,
+    uid,
+  });
+  try {
+    todo = await todo.save();
+    res.send(todo);
+  } catch (error) {
+    res.status(500).send(error.message);
+    console.log(error.message);
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const todo = await Todo.deleteOne(req.params.id);
+
+    res.send(todo);
+  } catch (error) {
+    res.status(500).send(error.message);
+    console.log(error.message);
+  }
+});
+
+module.exports = router;
