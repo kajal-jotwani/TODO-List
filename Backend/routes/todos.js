bbb08c4edbbed98564ff98647f65fa85ea7665a1
kajal-jotwani <kajaljotwani06@gmail.com>
2024-10-19 11:07:18 +0530

    updated get request

--- Backend/routes/todos.js
+++ Backend/routes/todos.js
@@ -1,9 +1,33 @@
 const {Todo} = require("../models/todo");
 const express = require("express");
+const Joi = require("joi");
+
 
 const router = express.Router();
 
+router.get("/", async(req, res) => {
+    try {
+        const todos = await Todo.find().sort({date: -1})
+        res.send(todos)
+    } catch(error){
+        res.status(500).send(error.message);
+        console.log(error.message)
+    }
+})
+
 router.post("/", async(req, res) =>{
+    const Schema  = Joi.object({
+        name: Joi.string().min(3).max(300).required(),
+        author: Joi.string().min(3).max(30),
+        uid: Joi.string(),
+        isComplete: Joi.boolean(),
+        date: Joi.date() 
+    })
+
+    const {error} = Schema.validate(req.body)
+
+    if(error) return res.status(400).send(error.details[0].message)
+   
     const {name, author, isComplete, date, uid} = req.body;
 
     let todo = new Todo({
