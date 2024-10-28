18e03c0c16556be5d30ba657b5af193b1fda5f2d
kajal-jotwani <kajaljotwani06@gmail.com>
2024-10-28 19:12:34 +0530

    signin api endpoint

---
+++ Backend/routes/signIn.js
@@ -0,0 +1,39 @@
+const Joi = require("joi");
+const jwt = require("jsonwebtoken");
+const express = require("express");
+const { User } = require("../models/user");
+const bcrypt = require("bcrypt");
+
+const router = express.Router();
+
+router.post("/", async (req, res) => {
+  const schema = Joi.object({
+    email: Joi.string().min(3).max(200).required().email(),
+    password: Joi.string().min(6).max(200).required(),
+  });
+  const { error } = schema.validate(req.body);
+
+  if (error) return res.status(400).send(error.details[0].message);
+
+  try {
+    let user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(400).send("Invalid email or password");
+
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!validPassword)
+      return res.status(400).send("Invalid email or password");
+
+    const SECRET_KEY = process.env.SECRET_KEY
+    const token = jwt.sign({_id: user._id, name: user.name, email: user.email}, SECRET_KEY)
+
+    res.send(token)
+  } catch (error) {
+    res.status(500).send(error.message);
+    console.log(error.message);
+  }
+});
+
+module.exports = router;
